f9cef98c2d77f9018a6587f15e7ef7c615559db9
Benny Border <Borde206@umn.edu>
2024-06-09 13:45:31 -0500

    version change

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='Skoal',
-    version='0.416',
+    version='0.417',
     packages=find_packages(),
     scripts=['skoal/main.py'],
     entry_points={
